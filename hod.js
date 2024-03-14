1fedddcbb4a3ed2ad84330b8ce1c850ac94e94e4
AbhishekUriti <106744521+AbhishekUriti@users.noreply.github.com>
2024-03-14 23:54:15 +0530

    Add files via upload

---
+++ hod.js
@@ -0,0 +1,39 @@
+const newVariable='hello world';
+console.log(newVariable)
+
+// let myBoolean=true
+// console.log(myBoolean)
+
+// let myString='Hello World';
+// console.log(myString)
+
+const firstNumber=20;
+console.log(firstNumber)
+
+var secondNumber=40
+var secondNumber=80
+console.log(secondNumber)
+
+// var myArray={myBoolean,myString}
+// console.log(myArray)
+
+let myBoolean = true; // Define the boolean variable
+let myString = 'Hello'; // Define the string variable
+
+let myArray1 = [myBoolean, myString]; // Create the array with the boolean and string variables
+
+console.log(myArray1); // Output the array
+
+
+
+let firstnumber=10;
+let secondnumber=20;
+let myArray=[true,'myString']
+let myObject={
+    firstProperty:myArray,
+    sumProperty:firstnumber+secondnumber
+};
+
+console.log(myObject);
+console.log(myObject.sumProperty)
+console.log(myObject.firstProperty)
---
+++ var.js
@@ -0,0 +1,15 @@
+var first_name="Abhishek";
+console.log(first_name)
+
+first_name="Dhoni";
+console.log(first_name[3])
+
+
+var thirdname="ghkkkkkkkkkkkkkkkkkkkkkkd";
+console.log(thirdname.length)
+console.log(thirdname[thirdname.length-1])
+
+
+let First_Name="Abhishek";
+let new_First=First_Name.slice(6,0)
+console.log(new_First)
